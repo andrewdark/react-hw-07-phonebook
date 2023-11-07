c167c4d22e0810e100c0bbc0fddb99e2bfab34ed
andrewdark <samsonov.a@ukr.net>
2023-11-07 09:51:35 +0200

    include components to app jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import React, { useEffect } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+import { ContactList } from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+//import { setContactList } from '../redux/store';
+
+import { fetchContacts } from '../redux/contacts/contactsOperations';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(state => state.contacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        width: 400,
+        marginLeft: '8px',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {isLoading && <b>Loading tasks...</b>}
+      {error && <b>{error}</b>}
+      <ContactFilter />
+      <ContactList />
     </div>
   );
 };
